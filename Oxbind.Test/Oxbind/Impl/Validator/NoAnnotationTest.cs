d77194879344a31c98f48c90c615b74abc01d56f
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2025-05-27 21:16:35 +0900

    Add test case for non attributed parameters

---
+++ Oxbind.Test/Oxbind/Impl/Validator/NoAnnotationTest.cs
@@ -0,0 +1,27 @@
+namespace Maroontress.Oxbind.Test.Oxbind.Impl.Validator;
+
+using System;
+using Maroontress.Oxbind.Impl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public sealed class NoAnnotationTest
+{
+    [TestMethod]
+    public void RootTest()
+    {
+        var logger = new Journal("Root");
+        var v = new Validator(typeof(Root), logger);
+        Assert.IsFalse(v.IsValid);
+        Assert.AreEqual(
+            """
+            Root: Error: A constructor parameter for a child element or text must be attributed with [Required], [Optional], [Multiple], or [ForText]: Extra, InnerText.
+            """,
+            string.Join(Environment.NewLine, logger.GetMessages()));
+    }
+
+    [ForElement("root")]
+    public record class Root(
+        string? Extra,
+        string InnerText);
+}
